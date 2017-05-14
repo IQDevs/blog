3f7b17e694fa13b59f025101e01f04bb300475c7
Fadi Hanna Al-Kass <AlKass@users.noreply.github.com>
2017-05-14 17:14:45 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ if [ $branch == "master" ]
 then
   gem install jekyll
   bundle install
-  git config --global user.email $4
-  git config --global user.name $3
   git clone https://$1:$2@github.com/iqdevs/iqdevs.github.io.git
   jekyll build --destination iqdevs.github.io
   cd iqdevs.github.io
+  git config --global user.email "$4"
+  git config --global user.name "$3"
   git add --all
   git commit -m "Updated on `date`"
   git push origin HEAD:master
