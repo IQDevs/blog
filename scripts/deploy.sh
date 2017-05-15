f74dd9ee5985abb11ecbf526162255d1de97dd3b
Fadi Hanna Al-Kass <AlKass@users.noreply.github.com>
2017-05-15 18:30:12 -0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,12 @@ if [ $branch == "master" ]; then
   git clone https://$1:$2@github.com/iqdevs/iqdevs.github.io.git
   jekyll build --destination iqdevs.github.io
   cd iqdevs.github.io
-  git config --global user.email "$4"
-  git config --global user.name "$3"
-  git add --all
-  git commit -m "Updated on `date`"
-  git push origin HEAD:master
+  ls -al
+  #git config --global user.email "$4"
+  #git config --global user.name "$3"
+  #git add --all
+  #git commit -m "Updated on `date`"
+  #git push origin HEAD:master
 else
   echo "branch is $branch. Nothing to be done here."
 fi
