73e682431acbd7e68287fdcd829d218440c261da
Fadi Hanna Al-Kass <AlKass@users.noreply.github.com>
2017-06-17 15:32:58 -0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ if [ $branch == "master" ]; then
   gem install jekyll
   bundle install
   git clone https://$1:$2@github.com/iqdevs/iqdevs.github.io.git
-  jekyll build --destination iqdevs.github.io --incremental --watch
+  jekyll build --destination iqdevs.github.io --incremental
   cd iqdevs.github.io
   git config --global user.email "$4"
   git config --global user.name "$3"
