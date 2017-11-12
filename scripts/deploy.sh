4dcc412d2c1aee0642431ba1917b6ad107f2b2a2
Fadi Hanna Al-Kass <eliya.3@wright.edu>
2017-11-12 15:50:26 -0600

    added missing

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+branch=`git branch | tail -n 1 | awk '{printf $1}'`
+
+if [ $branch == "master" ]; then
+  gem install jekyll
+  bundle install
+  git clone https://$1:$2@github.com/iqdevs/iqdevs.github.io.git
+  jekyll build --destination iqdevs.github.io --incremental
+  cd iqdevs.github.io
+  git config --global user.email "$4"
+  git config --global user.name "$3"
+  git status
+  git add --all
+  git status
+  git commit -m "Updated on `date`"
+  git push origin HEAD:master
+else
+  echo "branch is $branch. Nothing to be done here."
+fi
